f7c9b78f9c53291d8583e3e2a73c43e175980f76
otabeksherman <n.v.sherman@gmail.com>
2022-10-30 13:56:13 +0200

    add user class

---
+++ src/main/java/User.java
@@ -0,0 +1,13 @@
+class User {
+    private final int id;
+    private String email;
+    private String name;
+    private String password;
+
+    User(int id, String email, String name, String password) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+}
