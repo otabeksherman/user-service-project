6997ec3f67166bcec11a8ea240274c2ede691f20
otabeksherman <n.v.sherman@gmail.com>
2022-10-31 18:01:47 +0200

    services refactoring

--- src/main/java/App/service/AuthenticationService.java
+++ src/main/java/App/service/AuthenticationService.java
@@ -1,71 +1,50 @@
 package App.service;
-
-import App.Repository;
 import App.User;
 import App.utilities.UniqueNumber;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class AuthenticationService {
-    static HashMap<User, Long> userIdTokenMap = new HashMap<User, Long>;
 
-    // return optional token
-    public void loginUser(long id, String password) {
-        User user =  Repository.getUserById(id);
-        if (user == null) {
-            throw new RuntimeException("No user with given id");
+    private Repository repository = Repository.getInstance();
+    private HashMap<Long, Long> userIdToToken = new HashMap<>();
+    private UniqueNumber tokenUniqueNumber = new UniqueNumber();
+    private Set<Long> tokenSet = new HashSet<>();
+    private TokenService tokenService = new TokenService();
+
+    public void createUser(String name, String email, String password) {
+        Optional<List<String>> emails = repository.getEmails();
+
+        if (emails.isPresent()) {
+            if (emails.get().contains(email)) {
+                System.out.println(("User already registered with this email"));
+                return;
+            }
         }
-        UniqueNumber tokenUniqueNumber = new UniqueNumber();
+
+        UniqueNumber userId = new UniqueNumber();
+        long userIdNum = userId.getID();
+        User newUser = new User(userIdNum, name, email, password);
+        repository.writeUser(newUser);
+        System.out.println("user registered successfully");
+    }
+
+    public long logIn(String email, String password) {
+
+        Optional<User> user =  repository.getUserByEmail(email);
+
+        if (!user.isPresent()) {
+            System.out.println(String.format("User with email %s doesn't exists.", email));
+        }
+        if (!user.get().getPassword().equals(password)) {
+            System.out.println("Wrong password for given!");
+        }
+
         Long token;
-        if (!user.getPassword().equals(password)) {
-            throw new RuntimeException("wrong password for given");
-        }
-        token = tokenUniqueNumber.getID();
-        if (token == null) {
-            throw new RuntimeException("error while token was generated");
-        }
-        userLoginTokenMap(user, token);
-    }
-
-    private void userLoginTokenMap(User user, long token) {
-        userIdTokenMap.put(user.getId(), token);
-    }
-
-    private static void userLogout(User user) {
-        userIdTokenMap.remove(user.getId());
-    }
-
-    public static void updateName(User user, String name) {
-        updateUser(user, name, user.getEmail(), user.getPassword());
-    }
-
-    public static void updateEmail(User user, String email) {
-        updateUser(user, user.getName(), email, user.getPassword());
-    }
-
-    public static void userUpdatePassword(User user, String password) {
-        updateUser(user, user.getName(), user.getEmail(), password);
-    }
-
-    public static void updateUser(User user, String name, String email, String password) {
-        // need to check the token for this user
-        if (userIdTokenMap.containsKey(user.getId())) {
-            User user = Repository.getUserById(user.getId());
-            user.setEmail(email);
-            user.setName(name);
-            user.setPassword(password);
-            Repository.updateUserRepo(user);
-        } else {
-            throw new RuntimeException("This user is not logged in");
-        }
-    }
-
-    public static void deleteUser(User user, long token) {
-        if (userIdTokenMap.containsKey(user.getId())) {
-            userLogout(user);
-            Repository.deleteUserRepo(user);
-        } else {
-            throw new RuntimeException("This user is not logged in");
-        }
+        do {
+            token = tokenUniqueNumber.getID();
+        } while (tokenSet.contains(token));
+        tokenService.setSession(token, user.get().getId());
+        return token;
     }
 }
--- src/main/java/App/service/UserService.java
+++ src/main/java/App/service/UserService.java
@@ -1,18 +1,78 @@
 package App.service;
-import App.Repository;
+
 import App.User;
 import App.utilities.UniqueNumber;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 public class UserService {
-    public static User createUser(String name, String email, String password) {
-        if (Repository.getUserById.getEmail() != null)
-            throw new RuntimeException("User already registered with this email");
-        UniqueNumber userId = new UniqueNumber();
-        long userIdNum = userId.getID();
-        if (Repository.getUserById(userIdNum) != null)
-            throw new RuntimeException("User already registered with this id number");
-        User newUser = new User(userIdNum, name, email, password);
-        Repository.writeUser(newUser);
-        return newUser;
+
+    private Repository repository = Repository.getInstance();
+    private HashMap<Long, Long> userIdTokenMap = new HashMap<>();
+    private Set<Long> tokenSet = new HashSet<>();
+    private UniqueNumber tokenUniqueNumber = new UniqueNumber();
+    private Set<Long> loggedIdUsers = new HashSet<>();
+    private TokenService tokenService = new TokenService();
+
+    public void updateName(long id, long token, String newName) {
+        if (tokenService.tokenIsValid(token, id)) {
+            Optional<User> user = this.repository.getUserById(id);
+            if (user.isPresent()) {
+                User updatedUser = new User(user.get().getId(),
+                        newName, user.get().getEmail(), user.get().getPassword());
+                repository.updateUser(updatedUser);
+            } else {
+                System.out.println("Can't update name");
+            }
+        } else {
+            System.out.println("The token is invalid");
+        }
+    }
+
+    public void updateEmail(long id, long token, String newEmail) {
+        if (tokenService.tokenIsValid(token, id)) {
+            Optional<User> user = this.repository.getUserById(id);
+            if (user.isPresent() && !repository.getUserByEmail(newEmail).isPresent()) {
+                User updatedUser = new User(user.get().getId(),
+                        user.get().getName(), newEmail, user.get().getPassword());
+                repository.updateUser(updatedUser);
+            } else {
+                System.out.println("Can't update email");
+            }
+        } else {
+            System.out.println("The token is invalid");
+        }
+    }
+
+    public void userUpdatePassword(long id, long token, String newPassword) {
+        if (tokenService.tokenIsValid(token, id)) {
+            Optional<User> user = this.repository.getUserById(id);
+            if (user.isPresent()) {
+                User updatedUser = new User(user.get().getId(),
+                        user.get().getName(), user.get().getEmail(), newPassword);
+                repository.updateUser(updatedUser);
+            } else {
+                System.out.println("Can't update password");
+            }
+        } else {
+            System.out.println("The token is invalid");
+        }
+    }
+
+    public void deleteUser(long id, long token) {
+        if (tokenService.tokenIsValid(token, id)) {
+            Optional<User> user = repository.getUserById(id);
+            if (user.isPresent()) {
+                repository.deleteUser(user.get());
+
+            } else {
+                System.out.println("Can't delete user.");
+            }
+        } else {
+            System.out.println("The token is invalid");
+        }
     }
 }
